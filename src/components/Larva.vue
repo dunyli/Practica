5f00241ca2033990134230cacd695ba634d7c1f2
dunyli <dynuli01@mail.ru>
2025-06-28 20:23:04 +0700

    Добавление видов рыб в класс Larva

---
+++ src/components/Larva.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    class="larva"
+    :class="species"
+    :style="{
+      left: `${x}px`,
+      top: `${y}px`,
+      width: `${size}px`,
+      height: `${size}px`,
+      backgroundColor: larvaColor
+    }"
+    :title="`Личинка ${fishNames[species]}`"
+  ></div>
+</template>
+
+<script>
+export default {
+  props: {
+    x: Number,
+    y: Number,
+    size: Number,
+    species: String
+  },
+  data() {
+    return {
+      fishNames: {
+        pike: 'щуки',
+        silver_carp: 'толстолобика',
+        crucian: 'карася',
+        carp: 'карпа'
+      },
+      colors: {
+        pike: '#8a2be2',
+        silver_carp: '#7cfc00',
+        crucian: '#ffd700',
+        carp: '#ff8c00'
+      }
+    };
+  },
+  computed: {
+    larvaColor() {
+      return this.colors[this.species] || '#f9f06b';
+    }
+  }
+};
+</script>
+
+<style>
+.larva {
+  position: absolute;
+  border-radius: 50%;
+  transform: translate(-50%, -50%);
+  opacity: 0.9;
+}
+</style>
